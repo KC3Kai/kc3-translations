5013b60c43acdefcc20c83f5a8ee09cf02d1743c
dragonjet <jetriconew@gmail.com>
2015-11-16 08:09:18 +0800

    Translation Assistance @KC3Kai/translators There is a new "updater" folder Inside that, just open index.html Enter your language code, e.g. kr You can see the latest EN terms you havent translated with **** on it You can also edit on the spot and will become red if something is wrong Copy later on to the real .json file

---
+++ updater/scripts.js
@@ -0,0 +1,47 @@
+(function(){
+	
+	var EN_JSON = {};
+	var XX_JSON = {};
+	var NEW_JSON = {};
+	var i;
+	
+	$(document).on("ready", function(){
+		
+		EN_JSON = JSON.parse($.ajax({
+			async: false,
+			url: "https://raw.githubusercontent.com/KC3Kai/kc3-translations/master/data/en/terms.json",
+		}).responseText);
+		
+		for(i in EN_JSON){
+			EN_JSON[i] = "*****"+EN_JSON[i];
+		}
+		
+		$("#lang_submit").on("click", function(){
+			XX_JSON = {};
+			XX_JSON = JSON.parse($.ajax({
+				async: false,
+				url: "https://raw.githubusercontent.com/KC3Kai/kc3-translations/master/data/"+$("#lang_code").val()+"/terms.json",
+			}).responseText);
+			
+			
+			NEW_JSON = $.extend(true, EN_JSON, XX_JSON);
+			
+			console.log(EN_JSON, XX_JSON, NEW_JSON);
+			$("#newjson").val( JSON.stringify(NEW_JSON, null, "\t") );
+			$("#newjson").css("background", "#dfd");
+		});
+		
+		$(document).on("keyup", function(e){
+			var NEWTL = $("#newjson").val();
+			if(NEWTL==="") return true;
+			try {
+				JSON.parse(NEWTL);
+				$("#newjson").css("background", "#dfd");
+			}catch(ex){
+				$("#newjson").css("background", "#fdd");
+			}
+		});
+		
+	});
+	
+})();
